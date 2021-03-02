5b85eb00a2fc4abf0b4bd689b793fb712c4c4801
Brian Sahota <bpsahota@gmail.com>
2021-03-01 19:21:02 -0500

    getting there i likey

--- app/serializers/fav_good_serializer.rb
+++ app/serializers/fav_good_serializer.rb
@@ -1,3 +1,5 @@
 class FavGoodSerializer < ActiveModel::Serializer
   attributes :id, :good_id, :user_id, :note
+
+  belongs_to :good
 end
--- app/serializers/fav_recipe_serializer.rb
+++ app/serializers/fav_recipe_serializer.rb
@@ -1,3 +1,5 @@
 class FavRecipeSerializer < ActiveModel::Serializer
   attributes :id, :recipe_id, :user_id, :note
+
+  belongs_to :recipe
 end
